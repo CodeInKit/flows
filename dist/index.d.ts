86863ec8558f1bf7e7233f4efcd1aaa4c7289113
Amit Choukroun <amit@mtrn.co.il>
2021-01-08 21:58:50 +0200

    fix: remove dist from ignore for requirement on installation

---
+++ dist/index.d.ts
@@ -0,0 +1,56 @@
+export interface IActionData {
+    __flows?: {
+        flowName?: string;
+        jump?: string;
+        error?: Error;
+        done?: boolean;
+        requestId?: string;
+    };
+}
+interface IHookData {
+    flowName: string;
+}
+declare type Hook<T extends IHookData> = (hookData: T) => void;
+declare type Flow = ((data: IActionData, unsafe: {}) => IActionData | PromiseLike<IActionData>)[];
+export declare enum SupportedHooks {
+    pre_action = "pre_action",
+    post_action = "post_action",
+    pre_flow = "pre_flow",
+    post_flow = "post_flow",
+    exception = "exception"
+}
+export declare class Flows {
+    private hooks;
+    private flows;
+    constructor();
+    private getHook;
+    private getAction;
+    /**
+     * register flow
+     * @param {string} name the name of the flow
+     * @param {function[]} actions an array of functions
+     */
+    register(name: string, flow: Flow): void;
+    /**
+     *  add hook
+     * @param {SupportedHooks} name the name of the hook
+     * @param {Hook} fn the function to execute
+     */
+    hook(name: SupportedHooks, fn: Hook<IHookData>): void;
+    private isActionExists;
+    /**
+     * this method run recursively the flow in order to allow async based function and jump between flows.
+     *
+     * @param flowName the name of the flow
+     * @param data the data pass to the flow
+     * @param i the index number of the action in the flow
+     */
+    private executeRepeat;
+    /**
+     * start the execution process on a registered flow.
+     * @param {string} flowName
+     * @param {object} input
+     */
+    execute<T, S>(flowName: string, input: T, unsafe?: object): S | PromiseLike<S>;
+}
+export {};
---
+++ dist/index.js
@@ -0,0 +1,126 @@
+export var SupportedHooks;
+(function (SupportedHooks) {
+    SupportedHooks["pre_action"] = "pre_action";
+    SupportedHooks["post_action"] = "post_action";
+    SupportedHooks["pre_flow"] = "pre_flow";
+    SupportedHooks["post_flow"] = "post_flow";
+    SupportedHooks["exception"] = "exception";
+})(SupportedHooks || (SupportedHooks = {}));
+export class Flows {
+    constructor() {
+        this.hooks = new Map([
+            [SupportedHooks.pre_action, []],
+            [SupportedHooks.post_action, []],
+            [SupportedHooks.pre_flow, []],
+            [SupportedHooks.post_flow, []],
+            [SupportedHooks.exception, []]
+        ]);
+        this.flows = new Map();
+        this.executeRepeat = this.executeRepeat.bind(this);
+    }
+    getHook(hookName) {
+        const hook = this.hooks.get(hookName);
+        if (!Array.isArray(hook)) {
+            throw new Error(`Hook ${hookName} is not a known hook, please read the docs regarding acceptable hooks`);
+        }
+        return hook;
+    }
+    getAction(flowName, i) {
+        const flow = this.flows.get(flowName);
+        if (!Array.isArray(flow) || !flow[i]) {
+            throw new Error('flow does not exists!');
+        }
+        return flow[i];
+    }
+    /**
+     * register flow
+     * @param {string} name the name of the flow
+     * @param {function[]} actions an array of functions
+     */
+    register(name, flow) {
+        this.flows.set(name, flow);
+    }
+    /**
+     *  add hook
+     * @param {SupportedHooks} name the name of the hook
+     * @param {Hook} fn the function to execute
+     */
+    hook(name, fn) {
+        const hook = this.getHook(name);
+        hook.push(fn);
+    }
+    isActionExists(flowName, i) {
+        const flow = this.flows.get(flowName);
+        return Array.isArray(flow) && {}.toString.call(flow[i]) === '[object Function]';
+    }
+    /**
+     * this method run recursively the flow in order to allow async based function and jump between flows.
+     *
+     * @param flowName the name of the flow
+     * @param data the data pass to the flow
+     * @param i the index number of the action in the flow
+     */
+    async executeRepeat(flowName, data, unsafe, i, meta = { activated: [], requestId: '' }) {
+        const action = this.isActionExists(flowName, i) ? this.getAction(flowName, i) : null;
+        const actionData = JSON.parse(JSON.stringify(data));
+        let nextActionData = { __flows: actionData.__flows };
+        let lastFlow = meta.activated.length > 0 ? meta.activated[meta.activated.length - 1] : null;
+        if (actionData.__flows && actionData.__flows.requestId) {
+            meta.requestId = actionData.__flows.requestId;
+        }
+        if (flowName !== lastFlow && meta.activated.indexOf(flowName) === -1) {
+            meta.activated.push(flowName);
+        }
+        else if (flowName !== lastFlow) {
+            throw new Error(`cyclic flow!!, [${meta.activated.join(', ')}, ${flowName}]`);
+        }
+        /** post_flow hook */
+        if (!action || (actionData.__flows && actionData.__flows.done)) {
+            if (!actionData.__flows)
+                actionData.__flows = {};
+            actionData.__flows.requestId = meta.requestId;
+            this.getHook(SupportedHooks.post_flow).forEach(fn => fn({ flowName, output: actionData }));
+            return JSON.parse(JSON.stringify(actionData));
+        }
+        /** pre_action hook */
+        this.getHook(SupportedHooks.pre_action).forEach(fn => fn({ flowName, i, actionFn: this.getAction(flowName, i), input: actionData }));
+        try {
+            /** execution */
+            const result = await this.getAction(flowName, i)(actionData, unsafe);
+            if (typeof result !== 'object') {
+                throw new Error(`in flow ${flowName} action number ${i} return "${result}" instead of object!\nactions must return object`);
+            }
+            Object.assign(nextActionData, result);
+            /** exception hook */
+        }
+        catch (error) {
+            this.getHook(SupportedHooks.exception).forEach(fn => fn({ flowName, i, actionFn: this.getAction(flowName, i), input: actionData, error: error.message || error }));
+            throw error;
+        }
+        /** post_action hook */
+        this.getHook(SupportedHooks.post_action).forEach(fn => fn({ flowName, i, actionFn: this.getAction(flowName, i), input: actionData, output: nextActionData }));
+        /** next action */
+        if (nextActionData.__flows && nextActionData.__flows.jump) {
+            const jumpTo = nextActionData.__flows.jump;
+            delete nextActionData.__flows.jump;
+            return await this.executeRepeat(jumpTo, nextActionData, unsafe, 0, meta);
+        }
+        return await this.executeRepeat(flowName, nextActionData, unsafe, i + 1, meta);
+    }
+    /**
+     * start the execution process on a registered flow.
+     * @param {string} flowName
+     * @param {object} input
+     */
+    execute(flowName, input, unsafe) {
+        // We make sure that data is serializable
+        const data = JSON.parse(JSON.stringify(input));
+        if (!this.flows.has(flowName)) {
+            console.warn(`${flowName} flow does not exists! Skipped`);
+            return JSON.parse(JSON.stringify(input));
+        }
+        /** pre_flow hook */
+        this.getHook(SupportedHooks.pre_flow).forEach(fn => fn({ flowName: flowName, input: data }));
+        return this.executeRepeat(flowName, data, unsafe || {}, 0);
+    }
+}
